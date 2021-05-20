7ae8e9b10cafe0d92cfc63ef63297de94daadf5e
LucianoOga <luchovh17@gmail.com>
2021-05-20 16:05:21 -0300

    Solution of typing errors part 1000. Outcome with its  migration

--- db/migrations/00006_create_outcome_table.rb
+++ db/migrations/00006_create_outcome_table.rb
@@ -2,8 +2,8 @@ Sequel.migration do
     up do
       create_table(:outcomes) do
         primary_key   :id
-        foreing_key   :career_id, :careers
-        foreing_key   :choices_id, :choiches
+        foreign_key   :career_id, :careers
+        foreign_key   :choices_id, :choices
         DateTime      :created_at,   default: Sequel::CURRENT_TIMESTAMP
         DateTime      :updated_at,   default: Sequel::CURRENT_TIMESTAMP
       end
