e3f7bd7e4d4adb56d1973d698b8285ec796c1851
Rodri-M <rodricob777@gmail.com>
2021-05-20 14:23:15 -0300

    typing error fixed in outcome migration archive

--- db/migrations/00006_create_outcome_table.rb
+++ db/migrations/00006_create_outcome_table.rb
@@ -2,7 +2,7 @@ Sequel.migration do
     up do
       create_table(:outcomes) do
         primary_key   :id
-        foreing_key   :carrer_id, :carrers
+        foreing_key   :carrers_id, :carrers
         foreing_key   :choices_id, :choiches
         DateTime      :created_at,   default: Sequel::CURRENT_TIMESTAMP
         DateTime      :updated_at,   default: Sequel::CURRENT_TIMESTAMP
