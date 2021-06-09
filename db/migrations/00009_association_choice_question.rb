2f45478cf1789c3e3eb62b6b49a39c28a345ead3
Rodri-M <rodricob777@gmail.com>
2021-06-09 19:39:10 -0300

    uploaded migrations that are missing for the test to work correctly

---
+++ db/migrations/00009_association_choice_question.rb
@@ -0,0 +1,13 @@
+Sequel.migration do
+  up do
+    alter_table :choices do
+      add_foreign_key :question_id, :questions
+    end
+  end
+
+  down do
+    alter_table :choices do
+      drop_foreign_key :question_id
+    end
+  end
+end
---
+++ db/migrations/00010_association_response_choice.rb
@@ -0,0 +1,13 @@
+Sequel.migration do
+  up do
+    alter_table :responses do
+      add_foreign_key :choice_id, :choices
+    end
+  end
+
+  down do
+    alter_table :responses do
+      drop_foreign_key :choice_id
+    end
+  end
+end
