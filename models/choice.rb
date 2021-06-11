16a8a170045ac548e495c428f2bc0d571d1a395a
Lassalle Evelyn <evelassalla@gmail.com>
2021-06-10 22:21:14 -0300

    correccion

--- models/choice.rb
+++ models/choice.rb
@@ -4,19 +4,10 @@ class Choice < Sequel::Model
     many_to_one :question
     one_to_one  :response
 
-<<<<<<< HEAD
 	plugin :validation_helpers 
 	def validate
 		super 
 			validates_presence :text
 	end 
 end
-=======
-    plugin :validation_helpers 
-    def validate
-	super 
-		validates_presence :text
-    end  
-	
-end
->>>>>>> 5c1cce505bda316bf35d8786b6f1ccd73181d18b
+
